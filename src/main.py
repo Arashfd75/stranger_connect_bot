8fd8f513bc8bc8bdad4c8309ae267a6ed58acb1b
Arash <arash.fd75@gmail.com>
2021-10-13 18:29:35 -0600

    OOP code

--- src/main.py
+++ src/main.py
@@ -1,28 +1,36 @@
 import telebot
 import os
 from loguru import logger
-
+# print(os.environ['TelegramBotToken'])
+# bot = telebot.TeleBot(os.environ['TelegramBotToken'],parse_mode=None)
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+# 	bot.reply_to(message, "Howdy, how are you doing?")
+# print('starting bot')
+# bot.polling()
 class Bot:
     def __init__(self) -> None:
         self.bot = telebot.TeleBot(
             os.environ['TelegramBotToken'],
             parse_mode=None
+        )
 
-        self.send_welcome(self.bot.message_handler(commands=['start', 'help']))
-        
-@bot.message_handler(filters)
-def function_name(message):
-	bot.reply_to(message, "This is a message handler")
-    
+        self.send_welcome = self.bot.message_handler(commands=['start', 'help'])(self.send_welcome)
+
+
+    def send_welcome(self, message):
+	    bot.reply_to(message, "This is a message handler")
+
     def run(self):
         logger.info("Running Bot")
         self.bot.polling()
-    
-
-    if __name__ == '__main__':
-        Bot.run()
 
 
-) # You can set parse_mode by default. HTML or MARKDOWN
+if __name__ == '__main__':
+    bot = Bot()
+    bot.run()
+
+
+# You can set parse_mode by default. HTML or MARKDOWN
 
 
