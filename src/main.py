6ac9e72a5919574adee191872d2b7ef2def0a27c
Arash <arash.fd75@gmail.com>
2021-10-15 12:09:42 -0600

    stable build after adding constants and utils

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import telebot
 import os
 from loguru import logger
+from telebot import types
+from src.utils import create_keyboard
+from src.constants import keyboards
+
 # print(os.environ['TelegramBotToken'])
 # bot = telebot.TeleBot(os.environ['TelegramBotToken'],parse_mode=None)
 # @bot.message_handler(commands=['start', 'help'])
@@ -12,15 +16,32 @@ class Bot:
     def __init__(self) -> None:
         self.bot = telebot.TeleBot(
             os.environ['TelegramBotToken'],
-            parse_mode=None
+            parse_mode='HTML'
         )
 
         self.send_welcome = self.bot.message_handler(commands=['start', 'help'])(self.send_welcome)
+        self.respond_text = self.bot.message_handler(func=lambda message: True )(self.respond_text)
 
 
     def send_welcome(self, message):
-	    bot.reply_to(message, "This is a message handler")
+        self.set(message)
 
+        # markup = types.ReplyKeyboardMarkup(row_width=2)
+        # itembtn1 = types.KeyboardButton('a')
+        # itembtn2 = types.KeyboardButton('v')
+        # itembtn3 = types.KeyboardButton('d')
+        # markup.add(itembtn1, itembtn2, itembtn3)
+        self.bot.send_message(message.chat.id, "Choose", reply_markup=create_keyboard(['Connect', 'Settings']))
+        # self.bot.reply_to(message, f'Hii <b>{self.name}</b>!')
+
+    def respond_text(self, message):
+        self.set(message)
+        self.bot.send_message(message.chat.id, f"hiii {self.name}")
+
+    def set(self, message):
+        self.name = message.chat.first_name
+        if message.chat.last_name:
+            self.name = f'{self.name} {message.chat.last_name}'
     def run(self):
         logger.info("Running Bot")
         self.bot.polling()
