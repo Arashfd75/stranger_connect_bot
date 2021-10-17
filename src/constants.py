efe4b3ce51ce66412429547be2beee861c30df54
Arash <arash.fd75@gmail.com>
2021-10-17 17:59:57 -0600

    predefined texts

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,31 @@
 from utils import create_keyboard
 from types import SimpleNamespace
+
 KEYS = SimpleNamespace(**dict(
-    random_connect = 'Connect',
-    settings = 'Settings'
+    random_connect = ':satellite: Connect',
+    settings = ':gear: Settings',
+    back = ':cross_mark: Back',
+    disconnect = ':cross_mark: Disconnect'
 ))
 
-keyboards = dict(
-    main = create_keyboard([KEYS.random_connect, KEYS.settings])
+keyboards = SimpleNamespace(**dict(
+    main = create_keyboard([KEYS.random_connect, KEYS.settings]),
+    back=create_keyboard([KEYS.back]),
+    talking=create_keyboard([KEYS.disconnect])
+)
+)
+
+predefined_texts = SimpleNamespace(**dict(
+    welcome = 'To connect to an stranger click on the <b>Connect</b> button.',
+    init = "Please choose an option.",
+    disconnect = "you are disconnnected"
+)
+)
+
+states = SimpleNamespace(**dict(
+    init = "initializing",
+    random_connect = "waiting",
+    settings = "settings",
+    talking = 'talking'
+)
 )
